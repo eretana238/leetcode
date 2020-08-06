13ffa3d5abac0e4454f16ae442f0a5d646eda466
eretana238 <eretana238@gmail.com>
2020-08-05 20:35:38 -0600

    solved problem

--- algorithms/java/src/implement_trie/Trie.java
+++ algorithms/java/src/implement_trie/Trie.java
@@ -1,22 +1,54 @@
+import java.util.HashMap;
+
 public class Trie {
     /** Initialize your data structure here. */
+    class TrieNode {
+        HashMap<Character,TrieNode> child;
+        boolean endOfWord;
+
+        public TrieNode() {
+            child = new HashMap<Character,TrieNode>(26);
+            endOfWord = false;
+        }
+    }
+
+    private TrieNode root;
+
     public Trie() {
-        
+        root = new TrieNode();
     }
     
     /** Inserts a word into the trie. */
     public void insert(String word) {
-        
+        TrieNode current = root;
+        for(char c: word.toCharArray()) {
+            TrieNode node = current.child.getOrDefault(c, new TrieNode());
+            current.child.put(c, node);
+            current = node;
+        }
+        current.endOfWord = true;
     }
     
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        return false;
+        TrieNode current = root;
+        for(char c: word.toCharArray()) {
+            if(!current.child.containsKey(c))
+                return false;
+            current = current.child.get(c);
+        }
+        return current.endOfWord;
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        return false;
+        TrieNode current = root;
+        for(char c: prefix.toCharArray()) {
+            if(!current.child.containsKey(c))
+                return false;
+            current = current.child.get(c);
+        }
+        return true;
     }
 }
 /**
