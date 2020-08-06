ba0a58efc7d93ffa0935eb6f9dade3050fadea11
eretana238 <eretana238@gmail.com>
2020-08-06 14:16:27 -0600

    new leetcode question

---
+++ algorithms/java/src/add_and_search_word/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        WordDictionary obj = new WordDictionary();
+        obj.addWord("bad");
+        obj.addWord("dad");
+        obj.addWord("mad");
+        obj.addWord("dam");
+        // System.out.println(obj.search("pad"));
+        // System.out.println(obj.search("bad"));
+        // System.out.println(obj.search(".ad"));
+        // System.out.println(obj.search("b.."));
+        System.out.println(obj.search("..m"));
+    }
+}
