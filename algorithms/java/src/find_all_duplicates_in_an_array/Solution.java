81f2c33f1ddd7a4ac86e5e0dd9a9284bce8f99fa
eretana238 <eretana238@gmail.com>
2020-08-07 20:07:50 -0600

    finished problem #442

--- algorithms/java/src/find_all_duplicates_in_an_array/Solution.java
+++ algorithms/java/src/find_all_duplicates_in_an_array/Solution.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 442. Find All Duplicates in an Array
+ * 
  * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
  * Find all the elements that appear twice in this array.
  * Could you do it without extra space and in O(n) runtime?
